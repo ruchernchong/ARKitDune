bb24049468e21a16a5c704b47cecf5f66c35c6fc
Ru Chern Chong <me@ruchern.com>
2017-10-13 01:43:52 +0800

    Initial commit
    Signed-off-by: Ru Chern Chong <me@ruchern.com>

---
+++ arkit-dune/ViewController.swift
@@ -0,0 +1,105 @@
+import UIKit
+import SceneKit
+import ARKit
+
+class ViewController: UIViewController, ARSCNViewDelegate, ARSessionDelegate {
+    
+    var sceneNode : SCNNode?
+    let light = SCNLight()
+    
+    @IBOutlet var sceneView: ARSCNView!
+    @IBOutlet weak var start: UIButton!
+    
+    var session : ARSession {
+        return sceneView.session
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        sceneView.delegate = self
+        
+        sceneView.autoenablesDefaultLighting = false
+        sceneView.automaticallyUpdatesLighting = false
+        
+        let scene = SCNScene()
+        sceneView.scene = scene
+        
+        let configuration = ARWorldTrackingConfiguration()
+        configuration.isLightEstimationEnabled = true
+        session.run(configuration)
+        
+        UIApplication.shared.isIdleTimerDisabled = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        session.pause()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, updateAtTime time: TimeInterval) {
+        let estimate = session.currentFrame?.lightEstimate
+        
+        if estimate == nil {
+            return
+        }
+        
+        let intensity = estimate!.ambientIntensity / 40
+        light.intensity = intensity
+        sceneView.scene.lightingEnvironment.intensity = intensity
+        
+        print("Intensity \(intensity)")
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        guard let planeAnchor = anchor as? ARPlaneAnchor else { return }
+        
+    }
+    
+    func createBox() {
+        let box = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.0)
+        
+        //        let light = SCNLight()
+        light.type = .spot
+        light.spotOuterAngle = 20
+        light.color = UIColor.red
+        
+        let lightNode = SCNNode()
+        lightNode.light = light
+        lightNode.position = SCNVector3Make(0, 0, 1)
+        
+        self.sceneView.pointOfView?.addChildNode(lightNode)
+        
+        let material = SCNMaterial()
+        let grid = UIImage(named: "art.scnassets/checked.png")
+        //        material.lightingModel = .physicallyBased
+        material.diffuse.contents = grid
+        
+        let boxNode = SCNNode(geometry: box)
+        boxNode.geometry?.firstMaterial?.lightingModel = .physicallyBased
+        boxNode.eulerAngles = SCNVector3Make(45, 45, 0)
+        boxNode.position = SCNVector3Make(0, 0, -0.5)
+        boxNode.pivot = SCNMatrix4MakeRotation(Float.pi / 2, 1, 0, 0)
+        
+        let animation = CABasicAnimation(keyPath: "position.z")
+        animation.byValue = 1
+//        animation.toValue = SCNVector4(0, 0, 1, CGFloat(2 * Float.pi))
+        animation.timingFunction = CAMediaTimingFunction(name: kCAAnimationLinear)
+        animation.duration = 10
+        
+        boxNode.addAnimation(animation, forKey: "rotate")
+        
+        self.sceneView.pointOfView?.addChildNode(boxNode)
+    }
+    
+    @IBAction func start(_ sender: Any) {
+        createBox()
+    }
+}
+
