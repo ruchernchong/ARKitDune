64a4048e92a1da6c64b020822a663c82ba59c6b3
Ru Chern Chong <ruchern@sproud.biz>
2017-10-13 19:29:43 +0800

    Add scene

--- arkit-dune/ViewController.swift
+++ arkit-dune/ViewController.swift
@@ -5,10 +5,13 @@ import ARKit
 class ViewController: UIViewController, ARSCNViewDelegate, ARSessionDelegate {
     
     var sceneNode : SCNNode?
+    var animation : CAAnimation?
+    var longestDuration : Double? = 0
     let light = SCNLight()
     
     @IBOutlet var sceneView: ARSCNView!
     @IBOutlet weak var start: UIButton!
+    @IBOutlet weak var statusLabel: UILabel!
     
     var session : ARSession {
         return sceneView.session
@@ -19,11 +22,10 @@ class ViewController: UIViewController, ARSCNViewDelegate, ARSessionDelegate {
         
         sceneView.delegate = self
         
+        sceneView.antialiasingMode = .multisampling4X
+        
         sceneView.autoenablesDefaultLighting = false
         sceneView.automaticallyUpdatesLighting = false
-        
-        let scene = SCNScene()
-        sceneView.scene = scene
         
         let configuration = ARWorldTrackingConfiguration()
         configuration.isLightEstimationEnabled = true
@@ -49,50 +51,81 @@ class ViewController: UIViewController, ARSCNViewDelegate, ARSessionDelegate {
             return
         }
         
-        let intensity = estimate!.ambientIntensity / 40
+        let intensity = estimate!.ambientIntensity
         light.intensity = intensity
         sceneView.scene.lightingEnvironment.intensity = intensity
-        }
-    
-    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
-        guard let planeAnchor = anchor as? ARPlaneAnchor else { return }
-        
     }
     
-    func createBox() {
-        let box = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.0)
+    func placeObject() {
+        let scene = SCNScene(named: "art.scnassets/hangar.scn")!
+        let hangarNode = scene.rootNode.childNode(withName: "hangar", recursively: true)!
+        let gateNode = hangarNode.childNode(withName: "gate", recursively: true)!
         
-        light.type = .spot
-//        light.spotOuterAngle = 10
-        light.color = UIColor.red
+        hangarNode.enumerateChildNodes({ (child, stop) in
+            let animationKeys = child.animationKeys
+            
+            if !animationKeys.isEmpty {
+                for key: String in animationKeys {
+                    self.animation = child.animation(forKey: key)
+                    self.animation?.usesSceneTimeBase = false
+                    
+                    let duration = Double((animation?.duration)!)
+                    if duration > self.longestDuration! {
+                        self.longestDuration = duration
+                    }
+                }
+            }
+        })
+        
+        gateNode.enumerateChildNodes({ (child, stop) in
+            let animationKeys = child.animationKeys
+            if !animationKeys.isEmpty {
+                for key : String in animationKeys {
+                    self.animation = child.animation(forKey: key)
+                    self.animation?.usesSceneTimeBase = false
+                    
+                    let duration = Double((animation?.duration)!)
+                    
+                    if duration > self.longestDuration! {
+                        self.longestDuration = duration
+                    }
+                }
+            }
+        })
+        
+        self.sceneNode = hangarNode
+        self.sceneNode?.eulerAngles = SCNVector3Make(45, 0, 0)
+        self.sceneNode?.position = SCNVector3Make(0, 0, -2.5)
+        self.sceneView.pointOfView?.addChildNode(sceneNode!)
+        
+        light.type = .directional
+        light.spotOuterAngle = 45
+        light.color = UIColor.white
+        light.castsShadow = true
         
         let lightNode = SCNNode()
         lightNode.light = light
-        lightNode.position = SCNVector3Make(0, 0, 1)
+        lightNode.eulerAngles = SCNVector3Make(-45, 0, 0)
+        lightNode.position = SCNVector3Make(0, 1, 1)
         
         self.sceneView.pointOfView?.addChildNode(lightNode)
         
-        let boxMaterial = UIImage(named: "checkered.png")
-        
-        let boxNode = SCNNode(geometry: box)
-        boxNode.geometry?.firstMaterial?.diffuse.contents = boxMaterial
-        boxNode.geometry?.firstMaterial?.lightingModel = .physicallyBased
-        boxNode.eulerAngles = SCNVector3Make(45, 45, 0)
-        boxNode.position = SCNVector3Make(0, 0, -0.5)
-        boxNode.pivot = SCNMatrix4MakeRotation(Float.pi / 2, 1, 0, 0)
-        
-        let animation = CABasicAnimation(keyPath: "position.z")
-        animation.byValue = 1
-        animation.timingFunction = CAMediaTimingFunction(name: kCAAnimationLinear)
-        animation.duration = 10
-        
-        boxNode.addAnimation(animation, forKey: "rotate")
-        
-        self.sceneView.pointOfView?.addChildNode(boxNode)
     }
     
     @IBAction func start(_ sender: Any) {
-        createBox()
+        placeObject()
+        self.disableButton()
+
+        let timer = Timer.scheduledTimer(timeInterval: longestDuration!, target: self, selector: #selector(self.enableButton), userInfo: nil, repeats: false)
+    }
+    
+    func disableButton() {
+        self.start.isEnabled = false
+        self.start.setTitle("Started", for: .disabled)
+    }
+    
+    @objc func enableButton () {
+        self.start.isEnabled = true
+        self.start.setTitle("Click here to Start", for: .normal)
     }
 }
-
